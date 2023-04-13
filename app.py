6211660a5cb7315be421ca8035d6ec61aced5123
Cameron Leslie <cameronleslie@Camerons-MacBook-Pro.local>
2023-04-13 15:08:12 -0400

    app v4

--- app.py
+++ app.py
@@ -1,35 +1,31 @@
-from flask import Flask, jsonify, request
+import os
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-# Connect to the SQLite database
-engine = create_engine("sqlite:///covid.sqlite")
+# Set up the SQLite database connection
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///covid_data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-@app.route('/api/top_cases', methods=['GET'])
-def top_cases():
-    date = request.args.get('date', type=int)
-    
-    if not date:
-        return jsonify({'error': 'Date parameter is missing or invalid'}), 400
+# Render the index.html page
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-    query = f"""
-    SELECT state, cases
-    FROM (
-        SELECT state, SUM(cases) as cases
-        FROM "daily_cases_by_state"
-        WHERE date = {date}
-        GROUP BY state
-    )
-    ORDER BY cases DESC
-    LIMIT 10
-    """
+# Endpoint for fetching state data
+@app.route("/state_data/<state>")
+def state_data(state):
+    # Query the SQLite database for the specified state's data
+    state_table_name = f"{state} Aggregated"
+    query = f"SELECT date, cumulative_cases FROM \"{state_table_name}\";"
+    state_df = pd.read_sql_query(query, db.engine)
 
-    result = pd.read_sql_query(query, con=engine)
-    result_json = result.to_json(orient='records')
+    # Convert the data to a list of dictionaries and return as JSON
+    result = state_df.to_dict(orient="records")
+    return jsonify(result)
 
-    return result_json
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    app.run(debug=True, port = 5001)
--- dashboard.js
+++ dashboard.js
@@ -1,97 +1,78 @@
-async function fetchData(date) {
-    const response = await fetch(`/api/${date}`);
-    const data = await response.json();
-    return data;
-  }
-  function createDropdown() {
-    const dropdown = d3.select("#date-dropdown");
-  
-    for (let date = 1; date <= 420; date++) {
-      dropdown.append("option").text(date).property("value", date);
-    }
-  
-    dropdown.on("change", updateVisualization);
-  }
-  
-  // Update the updateVisualization function
-function updateVisualization() {
-    const selectedDate = d3.select("#date-dropdown").property("value");
-    Top10StateCases(selectedDate);
-  }
-  
-  // Create the Top10StateCases function
-  async function Top10StateCases(date) {
-    // Fetch the data for the selected date
-    const response = await fetch(`/api/date/${date}`);
-    const data = await response.json();
-  
-    // Sort the data by the number of cases
-    data.sort((a, b) => b.cases - a.cases);
-  
-    // Take the top 10 states
-    const top10States = data.slice(0, 10);
-  
-    // Display the horizontal bar chart
-    displayBarChart(top10States);
-  }
-  
-  // Create the displayBarChart function
-  function displayBarChart(data) {
-    const margin = { top: 20, right: 30, bottom: 40, left: 150 };
-    const width = 960 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
-  
-    // Remove the existing chart if any
-    d3.select("#bar-chart").remove();
-  
-    // Create the SVG element
-    const svg = d3.select("#visualization")
-      .append("svg")
-      .attr("id", "bar-chart")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
-  
-    // Set the scales
+// Get the reference to the dropdown menu
+const stateDropdown = d3.select("#stateDropdown");
+
+// Function to update the chart based on the selected state
+function updateChart() {
+  // Get the currently selected state abbreviation
+  const selectedState = stateDropdown.property("value");
+
+  // Get the data for the selected state from the Flask API
+  d3.json(`/state_data/${selectedState}`).then(data => {
+    // Prepare the data for the line chart
+    const chartData = data.map(d => {
+      return {
+        date: d.date,
+        cases: d.cumulative_cases
+      };
+    });
+
+    // Create the line chart
+    const svgWidth = 600;
+    const svgHeight = 400;
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const width = svgWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
+
+    // Remove the existing SVG if any
+    d3.select("#lineChart").select("svg").remove();
+
+    // Create a new SVG
+    const svg = d3.select("#lineChart")
+                  .append("svg")
+                  .attr("width", svgWidth)
+                  .attr("height", svgHeight);
+
+    const g = svg.append("g")
+                 .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Set up scales and axes
     const x = d3.scaleLinear().range([0, width]);
-    const y = d3.scaleBand().range([height, 0]).padding(0.1);
-  
-    x.domain([0, d3.max(data, d => d.cases)]);
-    y.domain(data.map(d => d.state));
-  
-    // Add the bars
-    svg.selectAll(".bar")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("x", 0)
-      .attr("height", y.bandwidth())
-      .attr("y", d => y(d.state))
-      .attr("width", d => x(d.cases));
-  
-    // Add the x-axis
-    svg.append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x));
-  
-    // Add the y-axis
-    svg.append("g")
-      .call(d3.axisLeft(y));
-  }
-  
-  // Initialize the dashboard
-  initDashboard();
-  
-  function initDashboard() {
-    // Create the date dropdown menu
-    createDropdown();
-  
-    // Update the visualization with the default date
-    updateVisualization();
-  }
-  
-  initDashboard();
-  
-        
+    const y = d3.scaleLinear().range([height, 0]);
+
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    x.domain(d3.extent(chartData, d => d.date));
+    y.domain([0, d3.max(chartData, d => d.cases)]);
+
+    // Create the line
+    const line = d3.line()
+                   .x(d => x(d.date))
+                   .y(d => y(d.cases));
+
+    // Append the line to the chart
+    g.append("path")
+     .datum(chartData)
+     .attr("fill", "none")
+     .attr("stroke", "steelblue")
+     .attr("stroke-linejoin", "round")
+     .attr("stroke-linecap", "round")
+     .attr("stroke-width", 1.5)
+     .attr("d", line);
+
+    // Append the x-axis
+    g.append("g")
+     .attr("transform", `translate(0, ${height})`)
+     .call(xAxis);
+
+    // Append the y-axis
+    g.append("g")
+     .call(yAxis);
+  });
+}
+
+// Add an event listener to the dropdown menu
+stateDropdown.on("change", updateChart);
+
+// Initialize the chart with the default state
+updateChart();
