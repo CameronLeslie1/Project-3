e9113aa7f57904091eca50dea550b535ac6be225
Cameron Leslie <cameronleslie@Camerons-MBP.lan>
2023-04-13 22:16:30 -0400

    v21

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 @app.route('/data')
 def get_data():
@@ -24,13 +24,11 @@ def get_data():
 
     return(jsonify(data))
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
 
 # Starting my app route
 
 @app.route('/data/<state>')
-def get_data(state):
+def get_state_data(state):
     conn = sqlite3.connect('covid.sqlite')
     cursor = conn.cursor()
 
@@ -48,4 +46,4 @@ def get_data(state):
     return(jsonify(data))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(port=5001)
